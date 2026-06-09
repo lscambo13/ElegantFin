2d3961c1262251315a63bf6ccca0e20f218c059a
lscambo13 <ls.cambo13@gmail.com>
2026-06-09 19:20:26 +0530

    dev: disabled ordering during minification; it didn't make a difference.

--- postcss.config.js
+++ postcss.config.js
@@ -5,15 +5,6 @@ module.exports = {
 	plugins: [
 		cssDeclarationSorter({
 			keepOverrides: true,
-			order: (a, b) => {
-				const aVar = a.startsWith('--');
-				const bVar = b.startsWith('--');
-
-				if (aVar && !bVar) return -1;
-				if (!aVar && bVar) return 1;
-
-				return a.localeCompare(b);
-			},
 		}),
 		cssnano({
 			preset: 'default',
